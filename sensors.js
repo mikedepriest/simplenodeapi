cdaf12ae4d41143ea4ea983c04a4a13593e070b4
Mike DePriest <mike.depriest@ey.com>
2018-06-27 06:07:26 -0500

    This configuration may actually work. Need to update the "Mode" entry in sensors.json to "" to run against real device on Pi.

--- sensors.js
+++ sensors.js
@@ -1,16 +1,19 @@
 fs = require('fs');
 
 var initialized = false;
+var testSensorDeviceFile = './testSensorDeviceFile';
 var sensorConfigFile = './sensors.json';
 var sensorConfig;
 var sensorList = [];
 var sensorDevicePath = '';
 var sensorDevicePathSuffix = '';
+var sensorMode = '';
 
 var init = function() {
     sensorConfig = JSON.parse(fs.readFileSync(sensorConfigFile, 'utf8'));
     sensorDevicePath = sensorConfig.SensorDevicePath;
     sensorDevicePathSuffix = sensorConfig.SensorDevicePathSuffix;
+    sensorMode = sensorConfig.Mode;
     sensorList = sensorConfig.SensorConfigs;
     initialized = true;
 };
@@ -56,13 +59,37 @@ exports.getSensorReadingById = function(id) {
     
     if (sensor != undefined) {
         var ts = new Date();
-        var sensorDeviceFile = sensorDevicePath+'/'+sensor.SensorId+'/'+sensorDevicePathSuffix;
-        var sensorReadingLine = fs.readFileSync(sensorDeviceFile, 'utf8');
+        var sensorDeviceFile = '';
+        if (sensorMode==='Test') {
+            sensorDeviceFile = testSensorDeviceFile;
+        } else {
+            sensorDeviceFile = sensorDevicePath+'/'+sensor.SensorId+'/'+sensorDevicePathSuffix;
+        }
+
+        var sensorReadingLine = new String();
+        sensorReadingLine = fs.readFileSync(sensorDeviceFile, 'utf8');
+        if (sensorReadingLine.includes('YES')) {
+            // The reading file looks like this:
+            //     85 01 4b 46 7f ff 0c 10 31 : crc=31 YES\r\n85 01 4b 46 7f ff 0c 10 31 t=24312
+            // The part we care about is the t=nnnnn, where nnnnn is temperature
+            // in 1000's of C (e.g. t=24312 is 24.312 C)
+            
+            // Eliminate any embedded linefeeds
+            sensorReadingLine.replace('\r','');
+            sensorReadingLine.replace('\n','');
+            // Split across '=' and take the third one as the reading
+            var readingArray = sensorReadingLine.split('=');
+            var reading = readingArray[2] / 1000;
+            // If the sensor UOM is F, convert
+            if (sensor.UOM==='F') {
+                reading = (1.8 * reading) + 32.0;
+            }
+        }
         retVal ={ 
             PublishTimestamp: ts.toISOString(),
             SensorId: sensor.SensorId,
             SensorName: sensor.SensorName,
-            Temperature: '0',
+            Temperature: reading,
             UOM: sensor.UOM 
         };
     }
