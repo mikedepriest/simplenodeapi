57878e850292d09f278edd36f2c9342f9012f5a5
Mike DePriest <mike.depriest@ey.com>
2018-07-16 05:19:02 -0500

    Added version to API, changed default route to human readable and added an /api machine readable route.

--- routes/routes.js
+++ routes/routes.js
@@ -7,49 +7,68 @@ var appRouter = function (app) {
         routes: 
         [
             { 
-                route: '/sensors',
+                route: '/v1/api',
+                description: 'List API calls',
+                returns: 'JSON array'
+            },
+            { 
+                route: '/v1/sensors',
                 description: 'List all configured sensors',
                 returns: 'JSON array'
             },
             {
-                route: '/sensors/id/:id',
+                route: '/v1/sensors/id/:id',
                 description: 'Provide information for a sensor by sensor ID',
                 returns: 'JSON representation of sensor'
             },
             {
-                route: '/sensors/name/:name',
+                route: '/v1/sensors/name/:name',
                 description: 'Provide information for a sensor by sensor name',
                 returns: 'JSON representation of sensor'
             },
-            {   route: '/sensorreadings',
+            {   route: '/v1/sensorreadings',
                 description: 'List sensor readings for all configured sensors',
                 returns: 'JSON array of sensor readings'
             },
             {
-                route: '/sensorreadings/id/:id',
+                route: '/v1/sensorreadings/id/:id',
                 description: 'Provide sensor reading for a sensor by ID',
                 returns: 'JSON representation of sensor reading'
             },
             {
-                route: '/sensorreadings/name/:name',
+                route: '/v1/sensorreadings/name/:name',
                 description: 'Provide sensor reading for a sensor by name',
-                returns: 'JSON representatoin of sensor reading'
+                returns: 'JSON representation of sensor reading'
             }
         ]
     };    
 
+    // Human readable API listing
     app.get("/", function (req, res) {
+        var output = 
+            '<h1>Temperature Sensor API</h1>'
+            +'<p>Version 1</p>'
+            +'<pre><code>'
+            +JSON.stringify(welcomeMessage,null,4)
+            +'</code></pre>';
+
+        res.set('Content-Type', 'text/html');
+        res.status(200).send(output);
+    });
+
+    // Machine readable API listing
+    app.get("/v1/api", function (req, res) {
         res.set('Content-Type', 'application/json');
         res.status(200).json({message: welcomeMessage});
     });
 
-    app.get("/sensors", function (req, res) {
+    app.get("/v1/sensors", function (req, res) {
         res.set('Content-Type', 'application/json');
         var mySensorList = Sensors.getSensorList();      
         res.status(200).json({sensorList: mySensorList});
     });
 
-    app.get("/sensors/id/:id", function (req, res) {
+    app.get("/v1/sensors/id/:id", function (req, res) {
         res.set('Content-Type', 'application/json');
         var mysensor;
         var mysensorId = req.params.id;
@@ -65,7 +84,7 @@ var appRouter = function (app) {
   
     });
 
-    app.get("/sensorreadings/id/:id", function (req, res) {
+    app.get("/v1/sensorreadings/id/:id", function (req, res) {
         res.set('Content-Type', 'application/json');
         var mysensor;
         var mysensorId = req.params.id;
@@ -81,7 +100,7 @@ var appRouter = function (app) {
   
     });
 
-    app.get("/sensors/name/:name", function (req, res) {
+    app.get("/v1/sensors/name/:name", function (req, res) {
         res.set('Content-Type', 'application/json');
         var mysensor;
         var mysensorName = req.params.name;
@@ -97,7 +116,7 @@ var appRouter = function (app) {
   
     });
 
-    app.get("/sensorreadings/name/:name", function (req, res) {
+    app.get("/v1/sensorreadings/name/:name", function (req, res) {
         res.set('Content-Type', 'application/json');
         var mysensor;
         var mysensorName = req.params.name;
@@ -113,7 +132,7 @@ var appRouter = function (app) {
   
     });
 
-    app.get("/sensorreadings", function (req, res) {
+    app.get("/v1/sensorreadings", function (req, res) {
         res.set('Content-Type', 'application/json');
         var mySensorReadingList = Sensors.getSensorReadings();      
         res.status(200).json({sensorreadings: mySensorReadingList});
